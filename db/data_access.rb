4c9d2248debba7f6211507b3ca4d9db7c855ce15
Naveen <navi.mehta@gmail.com>
2020-10-22 10:50:10 +1100

    added 6 new methods to data_access - fetch issues & projects for different status types. Updated index.erb to reflect project and issue summary

--- db/data_access.rb
+++ db/data_access.rb
@@ -13,3 +13,26 @@ def fetch_all_issues
     run_sql("SELECT * FROM issues;")
 end
 
+def fetch_issues_inprogress
+    run_sql("SELECT * FROM issues WHERE issue_status = 'in-progress';")
+end
+
+def fetch_issues_closed
+    run_sql("SELECT * FROM issues WHERE issue_status = 'closed';")
+end
+
+def fetch_issues_new
+    run_sql("SELECT * FROM issues WHERE issue_status = 'new';")
+end
+
+def fetch_projects_inprogress
+    run_sql("SELECT * FROM projects WHERE project_status = 'in-progress';")
+end
+
+def fetch_projects_closed
+    run_sql("SELECT * FROM projects WHERE project_status = 'closed';")
+end
+
+def fetch_projects_new
+    run_sql("SELECT * FROM projects WHERE project_status = 'new';")
+end
--- main.rb
+++ main.rb
@@ -5,13 +5,29 @@ require 'pry'
 require_relative 'db/data_access.rb'
 
 get '/' do
-  projects = fetch_all_projects
-  issues = fetch_all_issues
+  all_projects = fetch_all_projects
+  new_projects = fetch_projects_new
+  inprogress_projects = fetch_projects_inprogress
+  closed_projects = fetch_projects_closed
+
+  all_issues = fetch_all_issues
+  new_issues = fetch_issues_new
+  inprogress_issues = fetch_issues_inprogress
+  closed_issues = fetch_issues_closed
+
   erb :index, locals: {
-    projects: projects,
-    issues: issues
+    all_projects: all_projects,
+    new_projects: new_projects,
+    inprogress_projects: inprogress_projects,
+    closed_projects: closed_projects,
+    all_issues: all_issues,
+    new_issues: new_issues,
+    inprogress_issues: inprogress_issues,
+    closed_issues: closed_issues
   }
 end
+
+
 
 
 
