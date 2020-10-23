b0dccd7ad3ca0aaf279872e5c27b2768e4926f67
Naveen <navi.mehta@gmail.com>
2020-10-23 15:20:57 +1100

    added edit_project.erb

--- main.rb
+++ main.rb
@@ -5,15 +5,15 @@ require 'pry'
 require_relative 'db/data_access.rb'
 
 get '/' do
-  all_projects = fetch_all_projects
-  new_projects = fetch_projects_new
-  inprogress_projects = fetch_projects_inprogress
-  closed_projects = fetch_projects_closed
+  all_projects = fetch_all_projects()
+  new_projects = fetch_projects_new()
+  inprogress_projects = fetch_projects_inprogress()
+  closed_projects = fetch_projects_closed()
 
-  all_issues = fetch_all_issues
-  new_issues = fetch_issues_new
-  inprogress_issues = fetch_issues_inprogress
-  closed_issues = fetch_issues_closed
+  all_issues = fetch_all_issues()
+  new_issues = fetch_issues_new()
+  inprogress_issues = fetch_issues_inprogress()
+  closed_issues = fetch_issues_closed()
 
   erb :index, locals: {
     all_projects: all_projects,
@@ -37,7 +37,7 @@ post '/issues' do
 end
 
 get '/issues' do
-  all_issues = fetch_all_issues
+  all_issues = fetch_all_issues()
   erb :all_issues, locals: { all_issues: all_issues }
 end
 
@@ -46,12 +46,17 @@ get '/issues/:id/edit' do
   erb :edit_issue, locals: { issue: issue[0] }
 end
 
+patch '/issues/:id' do
+  update_issue(params["id"], params["issue_title"], params["issue_description"], params["project_id"], params["issue_status"], params["submitted_by"])
+  redirect '/issues'
+end
+
 get '/projects/new' do
   erb :create_project
 end
 
 get '/projects' do
-  all_projects = fetch_all_projects
+  all_projects = fetch_all_projects()
   erb :all_projects, locals: { all_projects: all_projects }
 end
 
@@ -60,12 +65,8 @@ post '/projects' do
   redirect '/projects'
 end
 
-patch '/issues/:id' do
-  update_issue(params["id"], params["issue_title"], params["issue_description"], params["project_id"], params["issue_status"], params["submitted_by"])
-  redirect '/issues'
-end
-
 patch '/projects/:id' do
   update_project(params["id"], params["project_title"], params["project_description"], params["project_owner"], params["project_status"], params["submitted_by"])
   redirect '/projects'
 end
+
